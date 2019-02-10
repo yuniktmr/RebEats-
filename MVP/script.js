295e924fab38e3c7086a5b8a525fa924b75faa06
Yunik Tamrakar <yuniktmr@gmail.com>
2019-02-09 23:00:57 -0600

    Basic homepage

---
+++ MVP/script.js
@@ -0,0 +1,25 @@
+/* global $ */
+/* global window */
+/* global document */
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    $('#toggle-btn').click(function () {
+                           $('.nav-links').slideToggle(1000);
+                           })
+})
+$(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if (scroll > 70){
+                $('#nav').addClass("new-nav");
+            }    else{
+                $('#nav').removeClass("new-nav");
+            }
+                 })
+$('nav a').click(function(link){
+    link.preventDefault();
+    let target = $(this).attr('href');
+    $('html','body').animate({
+        scrollTop : $(target).offset().top
+    }, "slow");
+})
