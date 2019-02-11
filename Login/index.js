8905ad090621b656cbb0d6432c03f11f4a033378
Marcus Higgins <halo2305@gmail.com>
2019-02-11 16:07:46 -0600

    started authentication

--- Login/index.js
+++ Login/index.js
@@ -1,5 +1,19 @@
 function login(){
-    alert.
-}
     
+
+
+}
+
+function register(){
+
+    var email = document.getElementById("emailInput").submit();
+    var password = document.getElementById("passwordInput").submit();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+        //TODO: error handling
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+
 }
