5d4075e0643088378a70aa00a090cbd9f521405d
Yunik Tamrakar <yuniktmr@gmail.com>
2019-03-30 13:48:03 -0500

    Update and delete the restaurants (owner view) with SQL injectiuon

--- RestaurantDB/View AND update/update.php
+++ RestaurantDB/View AND update/update.php
@@ -1,8 +1,13 @@
 <?php
     $con = mysqli_connect("127.0.0.1","root","toor");
     mysqli_select_db($con, 'eatrebs');
-    $sql = "UPDATE restaurant SET rest_name = '$_POST[rname]',email='$_POST[email]', address='$_POST[address]' WHERE rest_id = '$_POST[id]'";
-    
+    if(isset($_POST['del']))
+    {
+     $sql = "DELETE FROM restaurant  WHERE rest_id = '$_POST[id]'";
+    }
+    else{
+        $sql = "UPDATE restaurant SET rest_name = '$_POST[rname]',email='$_POST[email]', address='$_POST[address]' WHERE rest_id = '$_POST[id]'";
+    }
     mysqli_query($con,$sql);
     if (mysqli_query($con, $sql)){
         header('refresh:1; url="index.php"');
