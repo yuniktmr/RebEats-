0bf52dd5fd1032cd569d762f09784fb8515facab
Yunik Tamrakar <yuniktmr@gmail.com>
2019-03-30 15:03:45 -0500

    Update and delete the db entry for restaurants/ SQL injection now fixed

--- RestaurantDB/View AND update/update.php
+++ RestaurantDB/View AND update/update.php
@@ -1,20 +1,38 @@
 <?php
     $con = mysqli_connect("127.0.0.1","root","toor");
     mysqli_select_db($con, 'eatrebs');
+    $id = isset($_POST['id']) ? $_POST['id'] : '';
+    $name = isset($_POST['rname']) ? $_POST['rname'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $address = isset($_POST['address']) ? $_POST['address'] : '';
     if(isset($_POST['del']))
     {
-     $sql = "DELETE FROM restaurant  WHERE rest_id = '$_POST[id]'";
+        if ($sql = mysqli_prepare($con,"DELETE FROM restaurant  WHERE rest_id = ?")){
+            $sql -> bind_param("s",$id);
+            $sql -> execute ();
+            $sql -> close();
+            header('refresh:1; url="index.php"');
+        }
+        else{
+            echo"No update";
+        }
     }
     else{
-        $sql = "UPDATE restaurant SET rest_name = '$_POST[rname]',email='$_POST[email]', address='$_POST[address]' WHERE rest_id = '$_POST[id]'";
+        if ($sql = mysqli_prepare($con,"UPDATE restaurant SET rest_name = ?,email=?, address=? WHERE rest_id = ?")){
+            $sql ->bind_param("ssss", $name, $email, $address, $id);
+            $sql -> execute();
+            $sql -> close();
+            header('refresh:1; url="index.php"');
+        }
+        else{
+            echo "No update";
+        }
     }
-    mysqli_query($con,$sql);
-    if (mysqli_query($con, $sql)){
-        header('refresh:1; url="index.php"');
-    }else{
-        echo "No update";
-    }
+    
     
     
 
 ?>
+
+
+
